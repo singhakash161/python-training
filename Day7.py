b031e3cc37b2917d737ec4cfe8557feafdc3eaf3
singhakash161 <52224982+singhakash161@users.noreply.github.com>
2019-06-26 07:55:34 +0530

    Add files via upload

---
+++ Day7.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+#if statement-- new topic
+fruits = ['orange','banana','apple']
+
+for fruit in fruits:
+    if fruit == 'banana' :
+        
+        print(fruit.upper())
+        else:
+            print(fruit.title())
+        
+
+
+# In[ ]:
+
+
+
+
+
+# In[11]:
+
+
+#if statement-- new topic
+
+fruits = ['orange','banana','apple']
+
+for fruit in fruits:
+    if fruit == 'banana' :
+     print(fruit.upper())
+    else:
+     print(fruit.title())
+
+
+# In[12]:
+
+
+# rem python
+fruit = "banana"
+fruit.lower() == "banana"
+
+
+# In[13]:
+
+
+#== and what about not equal ? != ---not equality denotion
+shake = "Mango"
+if shake != "Banana" :
+    print("get Banana")
+
+
+# In[ ]:
+
+
+
+
