0535178e5ab175ac3a4368561ffbbb6d74ad2f40
singhakash161 <52224982+singhakash161@users.noreply.github.com>
2019-06-27 07:33:32 +0530

    Day8

---
+++ Day8.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+# Dictnories Introduction
+# It refer as mutable data types means value can be reassinged after declration
+## ---(key:value, key1:value1,....)
+
+
+# In[2]:
+
+
+#E.G.
+value1 ={'color':'green','point':5}
+print(value1['color'])
+print(value1['point'])
+
+
+# In[ ]:
+
+
+
+
