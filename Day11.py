68df8a34d371004638139d9cb59725a2e1f24fb8
singhakash161 <52224982+singhakash161@users.noreply.github.com>
2019-07-09 08:26:45 +0530

    Day11- Input Declaration

---
+++ Day11.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+#DECLARING USER INPUTS
+#how to achieve user inputs.. while running a program .. input()
+message = input("Feel free to ask, and will repond to you:")
+print (message)
+
+
+# In[2]:
+
+
+message = input("Feel free to ask, and will repond to you:")
+print (f"\n,{message}:")
+
+
+# In[3]:
+
+
+message = input("tell me your age:")
+print (f"\nyourage is,{message}:")
+
+
+# In[ ]:
+
+
+
+
